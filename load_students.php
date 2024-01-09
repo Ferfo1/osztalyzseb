932cfb57783c84a33f05b3b78a435016534eddc0
Ferfo1 <ferfo1hivatalos@gmail.com>
2024-01-09 16:27:01 +0100

    delete all

--- load_students.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    $data = file_get_contents('students.txt');
-    echo $data;
-} else {
-    echo 'Hiba: Érvénytelen kérés.';
-}
-?>
--- save_students.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = file_get_contents('php://input');
-    file_put_contents('students.txt', $data);
-    echo 'Adatok sikeresen mentve.';
-} else {
-    echo 'Hiba: Érvénytelen kérés.';
-}
-?>
